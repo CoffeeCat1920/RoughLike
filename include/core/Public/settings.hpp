d973a7d22158f303e633007ed26dae0bf7358dcf
CoffeeCat1920 <H0neyCatCoffeeGo@proton.me>
2026-02-28 13:25:08 +0500

    redone

---
+++ include/core/Public/settings.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+inline const int BLOCK = 32;
+
+inline const int BLOCK_X = 10;
+inline const int BLOCK_Y = 10;
+
+inline const int SCREEN_WIDTH = 10;
+inline const int SCREEN_HEIGTH = 10;
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "include/core/Public/settings.hpp"
+#include <raylib.h>
+
+int main() {
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGTH, "Quadtree Test");
+  SetTargetFPS(60);
+}
